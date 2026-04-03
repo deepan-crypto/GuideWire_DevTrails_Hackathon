ad34657e1f99e34019ee41c5e7eb0ce3f5331957
deepan-crypto <deepang467@email.com>
2026-04-03 18:08:33 +0530

    update in the ml response backend

--- backend/hackathon/src/main/java/com/dev_trails/hackathon/OracleLiveResponse.java
+++ backend/hackathon/src/main/java/com/dev_trails/hackathon/OracleLiveResponse.java
@@ -1,8 +1,13 @@
 package com.dev_trails.hackathon;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-@JsonIgnoreProperties(ignoreUnknown=true)
-public class OracleLiveResponse
- {
-public String zone;
-public Boolean payout_triggered;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class OracleLiveResponse {
+    public String zone;
+    public Double live_temp;
+    public Double live_rain;
+    public Double live_humidity;
+    public Double live_wind;
+    public Boolean payout_triggered;
+    public String trigger_type;
 }
